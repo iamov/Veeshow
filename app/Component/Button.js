4a4affe324c1a5df4410e830a8dce95262374680
Ominyi Jonah <jonahjohnzon@gmail.com>
2025-01-17 17:26:29 +0100

    update

--- app/Component/Button.js
+++ app/Component/Button.js
@@ -4,7 +4,7 @@ import { FaPlay } from "react-icons/fa";
 
 const Button = ({title, scal}) => {
   return (
-    <div className={` whitespace-nowrap bg-white font-semibold ${scal&&'scale-[120%]'} hover:scale-[110%] transition-all duration-300 ease-in-out flex items-center px-10 py-2 rounded-sm text-black`}>
+    <div className={` whitespace-nowrap bg-white font-bold ${scal&&'scale-[120%]'} hover:scale-[110%] transition-all duration-300 ease-in-out flex items-center px-10 py-3 rounded-sm text-black`}>
        <div className=' mr-3'><FaPlay/></div> <p>{title}</p></div>
   )
 }
--- app/Component/InfoButton.js
+++ app/Component/InfoButton.js
@@ -4,7 +4,7 @@ import { RiInformation2Line } from "react-icons/ri";
 
 const InfoButton = ({url}) => {
    return (
-     <div className=' whitespace-nowrap flex items-center px-10 py-2 font-semibold rounded-sm text-white border-white border-[1px]'>
+     <div className=' whitespace-nowrap flex items-center px-10 py-3 font-bold rounded-sm text-white border-white border-[1px]'>
         <div className=' mr-3'><RiInformation2Line/></div> <p>More Information</p></div>
    )
 }
