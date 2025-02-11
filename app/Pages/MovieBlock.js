b21a1c5de8dce9fb9c14e6a74582364547e6645a
jonahjohnzon <jonahjohnzon@gmail.com>
2025-02-11 11:18:33 +0100

    push

--- app/Pages/MovieBlock.js
+++ app/Pages/MovieBlock.js
@@ -16,7 +16,7 @@ const MovieBlock = ({data, passType = ""}) => {
 style: {transitionDelay: "1s"},
 }}>
     <div  role="img" aria-label="movie image" title="movie image"  className=' rounded-lg cursor-pointer overflow-hidden w-36 sm:w-48 h-60 sm:h-72 group relative ' onClick={()=>router.push(`/details/${type}/${data.id}/1`) } >
-      <img onError={addDefaultImg} className=' w-full h-full object-cover object-center absolute z-10 ' src={`${Api}${data.poster_path}`} loading='lazy' alt='image' />
+      <img onError={addDefaultImg} className=' w-full h-full object-cover object-center absolute z-10 ' src={`${Api}${data.poster_path}`} loading='lazy' alt={data.name || data.original_name || data.title} />
         <div className=' flex justify-center items-end bg-black w-full h-full absolute z-20 bg-opacity-0 group-hover:bg-opacity-80 transition-all duration-300 ease-in-out'>
           <div className=' px-2  font-bold w-full opacity-0   group-hover:opacity-100 transition-all text-center flex flex-col items-center absolute -bottom-10 group-hover:bottom-10 duration-300 ease-in-out'>
             <p className=' text-lg font-semibold'>{data.name || data.original_name || data.title}</p>
