5d7a032292d87ab9cce63c81fd46580d4a81c551
jonahjohnzon <jonahjohnzon@gmail.com>
2026-01-09 05:02:31 -0500

    push

--- app/sitemap.js
+++ app/sitemap.js
@@ -7,14 +7,14 @@ export default async function sitemap() {
   const api = new Apicore();
   const url = "https://api.themoviedb.org/3/trending/movie/day?language=en-US";
   const url1 = "https://api.themoviedb.org/3/trending/tv/day?language=en-US";
-  const telenovelaUrl = `${baseURL}/api/listseries?page=1`;
+  
 
   try {
     const res = await api.get(url);
     const sec = await api.get(url1);
-    const tel = await api.get(telenovelaUrl);
+   
 
-    if (!res?.results || !sec?.results || !tel?.series) throw new Error();
+    if (!res?.results || !sec?.results) throw new Error();
 
     const post = res.results.map((pos) => ({
       url: `${baseURL}/details/movie/${pos.id}/1`,
@@ -26,16 +26,11 @@ export default async function sitemap() {
       lastModified: new Date(pos.first_air_date || Date.now()).toISOString(),
     }));
 
-    const post3 = tel.series.map((pos) => ({
-      url: `${baseURL}/telenovela/${pos._id}`,
-      lastModified: new Date(pos.updatedAt || Date.now()).toISOString(),
-    }));
 
     return [
       { url: baseURL, lastModified: new Date().toISOString(), priority: 1 },
       ...post,
       ...post2,
-      ...post3,
     ];
   } catch (error) {
     console.error("Error generating sitemap:", error);
