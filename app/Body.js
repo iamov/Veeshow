99cdd22a91d05b895cf5eb9b63682fbc13b10418
jonahjohnzon <jonahjohnzon@gmail.com>
2026-01-09 16:05:54 -0500

    push

--- app/Body.js
+++ app/Body.js
@@ -14,7 +14,7 @@ import ReactGA from 'react-ga4'
 
 
 const dosis = Dosis({ subsets: ['latin'] })
-const tracking  = "GTM-PBB3SNNR"
+const tracking  = "G-7YGWTWFNZW"
 ReactGA.initialize(tracking)
 
 
