2700725e841fe7b50e94785a8a4235bde197e67d
iamov <ovofficial001@gmail.com>
2026-06-14 13:34:56 -0400

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -15,26 +15,26 @@ export default async function sitemap() {
     if (!res?.results || !sec?.results || !tel?.series) throw new Error();
 
     const post = res.results.map((pos) => ({
-      url: `https://letstream.site/details/movie/${pos.id}/1`,
+      url: `https://veewatch.com/details/movie/${pos.id}/1`,
       lastModified: new Date(pos.release_date || Date.now()).toISOString(),
     }));
 
     const post2 = sec.results.map((pos) => ({
-      url: `https://letstream.site/details/tv/${pos.id}/1`,
+      url: `https://veewatch.com/details/tv/${pos.id}/1`,
       lastModified: new Date(pos.first_air_date || Date.now()).toISOString(),
     }));
 
 
 
     return [
-      { url: "https://letstream.site", lastModified: new Date().toISOString(), priority: 1 },
+      { url: "https://veewatch.com", lastModified: new Date().toISOString(), priority: 1 },
       ...post,
       ...post2
     ];
   } catch (error) {
     console.error("Error generating sitemap:", error);
     return [
-      { url: "https://letstream.site", lastModified: new Date().toISOString(), priority: 1 },
+      { url: "https://veewatch.com", lastModified: new Date().toISOString(), priority: 1 },
     ];
   }
 }
